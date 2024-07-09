7fa9c89d4fc21732e614e18ff19ffaeb27d30d9d
Mario Yepes <marioy47@gmail.com>
2024-07-08 22:24:55 -0500

    Adding the 'format' option to return data in a table or in json

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 #!/usr/bin/env node
 
-import { Command, Argument } from "commander";
+import { Command, Argument, Option } from "commander";
 import noco from "./lib/noco.js";
 
 const program = new Command();
 program
   .command("noco")
   .description("Query the noco database")
+  .addOption(
+    new Option("--format <format>", "Output format in the console").choices([
+      "json",
+      "table",
+    ]),
+  )
   .addArgument(
     new Argument("<table>", "Table to query").choices(["accounts", "websites"]),
   )
   .argument("<query...>", "The string to query. PE `rust construction`")
-  .action((table, query) => {
+  .action((table, query, format) => {
     (async () => {
-      await noco(table, query);
+      await noco(table, query, format);
     })();
   });
 
