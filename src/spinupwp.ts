9664ad5de1d65b68890982217e8eb62f41104c24
Mario Yepes <marioy47@gmail.com>
2024-07-17 09:38:15 -0500

    Adding 'any' types in the code to remove errors in the IDE

--- src/spinupwp.ts
+++ src/spinupwp.ts
@@ -1,34 +1,21 @@
-interface SpWpServerData {
-  id: string;
-  name: string;
-  ip_address: string;
-  reboot_required: string;
-}
-export default async (
-  type: string,
-  query: string[],
-  { format }: { [key: string]: "table" | "json" },
-): Promise<void> => {
+// TODO: Remove all the 'any' types in favor of interfaces.
+
+export default async (type: string): Promise<void> => {
   const token = getEnvTokenOrExit();
-  let values = [];
+  let values: Object[] = [];
 
   switch (type.trim().toLowerCase()) {
     case "servers":
       values = await getServerData(token);
       break;
+    case "sites":
+      values = await getSitesData(token);
+      break;
   }
-  console.table(values, ["name", "ip", "rebootReq", "dash"]);
-  // switch (format?.trim().toLowerCase()) {
-  //   case "table":
-  //     console.table(values);
-  //     break;
-  //   default:
-  //     console.log(values);
-  //     break;
-  // }
+  console.table(values);
 };
 
-function getEnvTokenOrExit() {
+const getEnvTokenOrExit = (): string => {
   const token = process.env.SPINUP_WP_TOKEN;
   if (!token) {
     console.error(
@@ -37,23 +24,27 @@ function getEnvTokenOrExit() {
     process.exit(1);
   }
   return token;
-}
+};
 
-async function getServerData(token: string) {
-  const { data } = await fetchSpinupWpData(
+const getServerData = async (token: string) => {
+  const { data } = await fetchSpinupData(
     token,
     "https://api.spinupwp.app/v1/servers?limit=100",
   );
-  return data.map((item: { [key: string]: SpWpServerData }) => ({
+
+  return data.map((item : any) => ({
     id: item.id,
     name: item.name,
     ip: item.ip_address,
     rebootReq: item.reboot_required,
     dash: `https://spinupwp.app/keokee/servers/${item.id}`,
   }));
-}
+};
 
-async function fetchSpinupWpData(token: string, url: string) {
+const fetchSpinupData = async (
+  token: string,
+  url: string,
+) => {
   const data = await fetch(url, {
     method: "GET",
     headers: {
@@ -62,4 +53,33 @@ async function fetchSpinupWpData(token: string, url: string) {
     },
   });
   return await data.json();
-}
+};
+
+const getSitesData = async (token: string) => {
+  const servers = await getServerData(token);
+  const dashboardUrls: { [key: string]: [string, string] } = {};
+  const ids = servers.map((item:any) => {
+    dashboardUrls[item.id] = [
+      `https://spinupwp.app/keokee/servers/${item.id}`,
+      item.name,
+    ];
+    return fetchSpinupData(
+      token,
+      `https://api.spinupwp.app/v1/sites?limit=100&server_id=${item.id}`,
+    );
+  });
+  const allServers = await Promise.all(ids);
+  const allSites: Object[] = [];
+  allServers.forEach(({ data }) => {
+    const sites = data.map((item:any) => ({
+      id: item.id,
+      domain: item.domain,
+      server: dashboardUrls[item.server_id][1],
+      user: item.site_user,
+      serverDash: dashboardUrls[item.server_id][0],
+      siteDash: `https://spinupwp.app/keokee/sites/${item.id}`,
+    }));
+    allSites.push(...sites);
+  });
+  return allSites;
+};
